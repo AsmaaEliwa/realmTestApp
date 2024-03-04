c51a2bbb4711fd101bcbad8727abf38d364520b1
Asmaa Eliwa <asmaagamalelsayed96@gmail.com>
2024-03-04 11:03:32 +0200

    setting up

---
+++ realmTestApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  realmTestApp
+//
+//  Created by asmaa gamal  on 04/03/2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
